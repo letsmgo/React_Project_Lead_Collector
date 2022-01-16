4abf80e39ce2f86c7acd13dcd3bae3136d6473f9
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-16 20:46:45 -0300

    feat: implement Header component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/header/Header'
 
 ReactDOM.render(
-
+  <Header title="Lead Collector login"/>
+  ,
   document.getElementById('root')
-  
 );
 
