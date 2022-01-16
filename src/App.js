aded527ba95bd96e61734502eca1fca161a1c436
Let Marçal <leticiamarcalmatias@gmail.com>
2022-01-16 20:57:58 -0300

    refactor: login component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React, {Component} from "react";
+import Login from "./components/pages/Login";
+import "./App.css"
 
+
+class App extends Component {
+    render(){
+        return(
+            <div className="App">
+                <Login/>
+            </div>
+        );
+    }
+}
+
+export default App 
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
 import {Form, FormGroup, Label, Input, Button} from 'reactstrap'
+import Header from '../header/Header';
 
 export default class Login extends Component {
     render() {
         return(
+            <div>
+            <Header title="Página de login"/>
+            <hr />
             <Form>
                 <FormGroup>
                     <Label for="email"> Email</Label>
@@ -13,8 +17,9 @@ export default class Login extends Component {
                     <Label for="password"> Senha</Label>
                     <Input type="password" id="password" placeholder="Informe a sua senha: "/>
                 </FormGroup> 
-                <Button color="primary"> Entrar </Button>
+                <Button color="primary" block> Entrar </Button>
             </Form>
+        </div>
         );
     }
 } 
